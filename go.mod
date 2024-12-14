ef8765d55e2444b72637bbbe609565ce4cf28bc2
Maruqes <m4rques1s0n@gmail.com>
2024-12-14 22:08:51 +0000

    add

---
+++ go.mod
@@ -0,0 +1,12 @@
+module motoClubeMourosBackend
+
+go 1.23.3
+
+require (
+	github.com/Maruqes/Tokenize v0.2.5 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+	github.com/stripe/stripe-go/v81 v81.0.0 // indirect
+	golang.org/x/crypto v0.29.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/Maruqes/Tokenize"
+	login "github.com/Maruqes/Tokenize/Login"
+	types "github.com/Maruqes/Tokenize/Types"
+)
+
+func testLogado(w http.ResponseWriter, r *http.Request) {
+	if !login.CheckToken(r) {
+		w.Write([]byte("Não Logado"))
+		return
+	}
+	w.Write([]byte("Logado"))
+}
+
+func testPago(w http.ResponseWriter, r *http.Request) {
+	if !login.CheckToken(r) {
+		w.Write([]byte("Não Logado"))
+		return
+	}
+	w.Write([]byte("Pago"))
+}
+
+func main() {
+
+	http.HandleFunc("/logado", testLogado)
+	http.HandleFunc("/pago", testPago)
+
+	Tokenize.Init("4242", "/sucess", "/cancel", types.TypeOfSubscriptionValues.Normal, []types.ExtraPayments{types.ExtraPaymentsValues.Multibanco})
+}
